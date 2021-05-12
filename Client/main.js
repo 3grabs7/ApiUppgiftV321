cfcb998cb862b94c80275d60845c9282cc381d64
Robin Grabs <robinmgrabs@gmail.com>
2021-05-12 23:10:41 +0200

    clean

--- Client/main.js
+++
@@ -1,7 +0,0 @@
-import { map, loadMap, getLonLat } from '/map.js'
-window.onload = loadMap()
-map.addEventListener('click', (e) => {
-	const lon = (e.clientX - rect.left) * px2lon + topLeft.lon
-	const lat = (e.clientY - rect.top) * px2lat + topLeft.lat
-	const lonlat = getLonLat(lon, lat)
-})
